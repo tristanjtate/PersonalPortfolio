21350210e17d540f72537d370bf679b0b71bcc3c
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-10-06 13:34:42 -0700

    added skills section bar for skills in progress into html, css styling and also js  to add eventlistener once page is fully loaded. I loop through skill bars to set width for respective percentages

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-// off-screen menu and hamMenu
+// off-screen menu and hamMenu and menu items
 
 // variables to select by class
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const menuItems = document.querySelectorAll(
+  ".off-screen-menu li"
+); /*all menu items*/
 
 // event listener for clicks on hamMenu and when click is detected
 // we want to toggle the 'active' class for both hamMenu and offScreenMenu
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamMenu.classList.remove(
+      "active"
+    ); /*removes active class for menu appearing*/
+    offScreenMenu.classList.remove("active");
+  });
+});
+
+// animate skills
+
+document.addEventListener("DOMContentLoaded", () => {
+  const skillBars = document.querySelectorAll(".skill-percent");
+
+  skillBars.forEach((bar) => {
+    const percentage = bar.getAttribute("data-per");
+    bar.style.width = percentage; // Correctly apply the percentage width
+  });
 });
