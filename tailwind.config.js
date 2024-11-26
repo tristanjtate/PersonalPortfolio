90db382d46c56b3d43453ec31c7cb6563243b514
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-11-26 15:56:44 -0700

    updated index.html with structure up until start of (about me)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+     'darkTurq' : '#003135',
+     'medTurq' : '#024950',
+     'orangeRed' : '#964734',
+     'turq' : '#0FA4Af',
+     'mySkyBlue' : '#AFDDE5'
+    }
   },
   plugins: [],
 };
