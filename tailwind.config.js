61ba5f39ad2be5af28328c6acd814f6ef374e2e1
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-12-05 13:02:35 -0700

    updated up until skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-     'darkTurq' : '#003135',
-     'medTurq' : '#024950',
-     'orangeRed' : '#964734',
-     'turq' : '#0FA4Af',
-     'mySkyBlue' : '#AFDDE5'
-    }
+    extend: {
+      colors: {
+        customDarkBlue: '#003135',
+        customBlue: '#024950',
+        customRed: '#964734',
+        customTeal: '#0fa4af',
+        customLightBlue: '#afDDe5',
+      },
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+        parkinsans: ['Parkinsans', 'sans-serif'],
+        spacemono: ['Space Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
