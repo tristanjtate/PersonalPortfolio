f28d4a7e2d4ee5b85146eec5983159cfb5de7688
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-12-05 15:57:52 -0700

    updated tailwindcss for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customDarkBlue: '#003135',
-        customBlue: '#024950',
-        customRed: '#964734',
-        customTeal: '#0fa4af',
-        customLightBlue: '#afDDe5',
+        customDarkBlue: "#003135",
+        customBlue: "#024950",
+        customRed: "#964734",
+        customTeal: "#0fa4af",
+        customLightBlue: "#afDDe5",
       },
       fontFamily: {
-        overpass: ['Overpass', 'sans-serif'],
-        parkinsans: ['Parkinsans', 'sans-serif'],
-        spacemono: ['Space Mono', 'monospace'],
+        overpass: ["Overpass", "sans-serif"],
+        parkinsans: ["Parkinsans", "sans-serif"],
+        spacemono: ["Space Mono", "monospace"],
+      },
+      keyframes: {
+        pulsescale: {
+          "0%, 120%": { opacity: 1, transform: "scale(1)" },
+          "50%": { opacity: 0.7, transform: "scale(1.1)" },
+        },
+      },
+      animation: {
+        pulsescale: "pulsescale 2s ease-in-out infinite", // 3 seconds per cycle
       },
     },
   },
