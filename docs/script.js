3faa5b58005f7b752c674dad7d1098e8cd456dbf
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-12-10 10:49:42 -0700

    added requirements.txt for backend, and js logic for form

---
+++ docs/script.js
@@ -0,0 +1,37 @@
+// get the form and response div
+
+const form = document.getElementById('contactForm');
+const responseDiv = document.getElementById('responseMessage');
+
+// add an event listener to the form
+form.addEventListener('submit', function (e) {
+    // this will stop the form from submitting and refreshing the page
+    e.preventDefault();
+});
+
+// getting form data
+// this grabs all form fields and their values
+const formData = new FormData(form);
+
+// send data using fetch to backend URL
+fetch('need to add the backend URL here', {
+    method: 'POST',
+    body: formData
+})
+    .then((response) => response.text())
+    .then((data) => {
+        // data is the responsve from the backend (like success message)
+        responseDiv.style.display = 'block';
+        responseDiv.textContent = data; // this will show the response as text
+        // now resetting form
+        form.reset();
+    })
+    .catch((error) => {
+        // if there is an error, show it in the response div
+        responseDiv.style.display = 'block';
+        responseDiv.style.color = 'red';
+        responseDiv.textContent = 'Sorry, something went wrong. Please try again later.';
+        // also print the error in the console
+        console.error("Error", error);
+    });
+
